a41eec7b51328f7aea11ee0f60a97c15dc89bb29
Nura Hassan <nuramhassan2@gmail.com>
2023-12-11 18:55:11 +0000

    form

---
+++ packages/frontend/src/components/ReviewList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ReviewList = () => {
+    return (
+        <div>
+            <h2>
+                Reviews
+            </h2>
+
+        </div>
+    )
+}
+
+export default ReviewList;

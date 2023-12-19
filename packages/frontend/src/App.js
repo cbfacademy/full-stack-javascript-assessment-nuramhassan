02d69942f5d3bdb4027bc950e811ddf0fc0b33f9
Nura Hassan <nuramhassan2@gmail.com>
2023-12-19 22:53:55 +0000

    set up mongodb connection and added header

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -2,12 +2,38 @@ import React from "react";
 import "./App.css";
 import Form from "./components/Form";
 
+function Header(){
+  return (
+    <nav className="nav-bar">
+      <p>Logo</p>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li> 
+          <a href="/contact—us">Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+  )
+
+}
+
 function App() {
-  return (<div className="App">
-    <h1> Welcome to Rainbow Noir! </h1>
-    <Form />
-  </div>)
-  
+     
+  return (
+      <>
+        <Header />
+        <div className="App">
+          <div> <h1 className='text-xl'> Welcome to Rainbow Noir! </h1>
+          </div>
+      
+          <Form />
+      
+        </div>      
+      </>      
+      );
 }
 
 export default App;
